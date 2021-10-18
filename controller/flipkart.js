f7a3c704520a1f482832be08c86d659939afb80c
ayush tyagi <ayushtyagi6395@gmail.com>
2021-10-18 17:28:49 +0530

    webScraper task 4

--- controller/flipkart.js
+++ controller/flipkart.js
@@ -7,7 +7,7 @@ const flipkartScraper = async (req, res) => {
     function (err, res, body) {
       if (!err && res.statusCode == 200) {
         const $ = cheerio.load(body);
-        const data= $("div._1AtVbE > div._13oc-S").each((key, element) => {
+        const data = $("div._1AtVbE > div._13oc-S").each((key, element) => {
           console.log(key + 1);
           const PhoneName = $(element).find("div._4rR01T").html();
           console.log(PhoneName);
@@ -17,7 +17,6 @@ const flipkartScraper = async (req, res) => {
           console.log(TotalRatingsAndReviews);
           const PhonePrice = $(element).find("div._30jeq3").text();
           console.log(PhonePrice);
-          
         });
       } else {
         console.log("error");
@@ -25,32 +24,36 @@ const flipkartScraper = async (req, res) => {
     }
   ); res.send("data fetched")
 };
-const flipkartScraperFull=async(req,res)=>{  
-        //  WORKING ON IT
-  // request.get(
-  //   "https://www.flipkart.com/mobiles/pr?sid=tyy,4io&p[]=facets.brand%255B%255D%3DPOCO",
-  //   function (err, res, body) {
-  //     if(!err && res.statusCode==200){
-  //       const $=cheerio.load(body)
-  //       const data=$("div._1AtVbE > div._13oc-S").each((key,element)=>{
-  //         const link=$(element).find("a._1fQZEK").attr('href')
-  //         // console.log(key+1)
-  //         request.get(`https://www.flipkart.com${link}`,function(req,res){
-  //          const $= cheerio.load(body)
-  //          const ab=$("div._1YokD2._3Mn1Gg").find("h1.yhB1nd").text()
-  //          console.log(ab)
-  //         //  const name= $(element).find("h1.yhB1nd > span.B_NuCI").html()
-  //         // const price= $(element).find("div._30jeq3").text()
-  //         // console.log(name)
-  //         // console.log(price)
-  //         })
-  //         // console.log(link)
-  //       })
-  //       // console.log(data)
-        
-  //     }
-  //   }
-  // )
-   res.send("flipkartScraperFull")
+const flipkartScraperFull = async (req, res) => {
+  request.get(
+    "https://www.flipkart.com/mobiles/pr?sid=tyy,4io&p[]=facets.brand%255B%255D%3DPOCO",
+    function (err, res, body1) {
+      if (!err && res.statusCode == 200) {
+        const $ = cheerio.load(body1)
+        $("div._1AtVbE > div._13oc-S").each((key, element) => {
+          const link = $(element).find("a._1fQZEK").attr('href')
+          request.get(`https://www.flipkart.com${link}`, function (request, response, body2) {
+            const $ = cheerio.load(body2)
+            const subData1 = $("div._1YokD2 > div._3Mn1Gg > div._1AtVbE > div.aMaAEs")
+            const phoneName = $(subData1)
+              .find("span.B_NuCI").text()
+            const phonePrice = $(subData1).find("div._30jeq3").text()
+            subData2 = $("div._1YokD2 > div._3Mn1Gg > div._1AtVbE")
+            const offers = $(subData2).find("div.XUp0WS").text()
+            const highlights = $("div._1YokD2 > div._3Mn1Gg.col-8-12 > div._1YokD2 ")
+              .find("div._2cM9lP").text()
+            console.log(`Phone name is ${phoneName}`)
+            console.log(`Price ${phonePrice}`)
+            console.log(highlights)
+            console.log(`${offers}
+             `)
+          })
+        })
+      } else {
+        console.log("error exists")
+      }
+    }
+  )
+  res.send("flipkartScraperFull")
 }
-module.exports = {flipkartScraper,flipkartScraperFull};
+module.exports = { flipkartScraper, flipkartScraperFull };
